46c859796e5485f96ee10daef7a59856ba43dbea
zyndev <zyndev@gmail.com>
2018-03-18 00:40:13 +0800

    add task and asset

--- devops/urls.py
+++ devops/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+import member
+import projects
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^member/', include('member.urls')),
+    url(r'^project/', include('projects.urls')),
     url(r'^$', views.index),
     url(r'^login$', views.login, name='login'),
     url(r'^logout$', views.logout, name='logout'),
--- member/models.py
+++ member/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=30)
+    status = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField()
+    update_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'department'
+
+
+class Member(models.Model):
+    name = models.CharField(max_length=30)
+    department = models.ForeignKey(Department, models.DO_NOTHING, blank=True, null=True)
+    position = models.ForeignKey('Position', models.DO_NOTHING, blank=True, null=True)
+    email = models.CharField(max_length=40, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField()
+    update_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'member'
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=30)
+    status = models.IntegerField(blank=True, null=True)
+    create_time = models.DateTimeField()
+    update_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'position'
