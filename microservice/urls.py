a4b77fb67fcdf38b32fc29ee6c6e9838e2039723
zyndev <zyndev@gmail.com>
2018-03-28 10:50:34 +0800

    modify requirements

--- microservice/urls.py
+++ microservice/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="microservice/index"),
-    url(r'^add$', views.add, name="microservice/add"),
+    path(r'', views.index, name="microservice/index"),
+    path(r'add', views.add, name="microservice/add"),
 ]
